e72f52d94eb6d17c7a9e49de3b53e66209ee0e2c
Nat <nat.wrw@gmail.com>
2020-04-08 20:06:53 +0700

    add dev server proxy

--- frontend/src/vue.config.js
+++ frontend/src/vue.config.js
@@ -2,6 +2,15 @@ module.exports = {
     "transpileDependencies": [
         "vuetify"
     ],
+    devServer: {
+        proxy: {
+            '/': {
+                target: 'http://localhost:39112/',
+                'changeOrigin': true,
+                'secure': false
+            }
+        }
+    },
     // outputDir: './dist/',
     assetsDir: 'static',
 };
