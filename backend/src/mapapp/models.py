a1be147929342b7f39b13f021a685b10dff51bbb
Pawat Saengduan <49898465+pawatsaengduan@users.noreply.github.com>
2020-04-10 17:38:40 +0700

    Prepare code for image

--- backend/src/mapapp/models.py
+++ backend/src/mapapp/models.py
@@ -3,14 +3,20 @@ from django.db import models
 CATEGORY = (
     ("covid19", "COVID-19: โรงทานน้ำใจ"),
     ("rescue", "Rescue"),
-    ("er-hospital", "ER-Hospital"),
+    ("erhospital", "ER-Hospital"),
     ("aed", "AED")
 )
+
+count = 0
 # Create your models here.
 
 
+def get_image_filename(instance, filename):
+    id = instance.post.id
+    return "/media/images" % (id)
+
+
 class Map(models.Model):
-    id = models.IntegerField(primary_key=True, help_text='Primary Key')
     place_name = models.CharField("Place Name", max_length=100)
     place_categories = models.CharField(
         "Place Categories", max_length=11, choices=CATEGORY)
@@ -18,11 +24,16 @@ class Map(models.Model):
         "Place Description", max_length=2000, blank=True)
     place_enable = models.BooleanField(
         "Is Place Enable?",  default=True)
-    place_latitude = models.FloatField(
+    place_latitude = models.CharField(
         "Place Latitude", max_length=50)
-    place_longitude = models.FloatField(
+    place_longitude = models.CharField(
         "Place Longitude", max_length=50)
-    place_image = models.ImageField("Place Image", blank=True)
 
     def __str__(self):
         return str(self.place_name)
+
+
+# class Map_Image(models.Model):
+#     property = models.ForeignKey(
+#         Map, related_name='images', on_delete=models.CASCADE)
+#     place_image = models.ImageField("Place Image", blank=True)
