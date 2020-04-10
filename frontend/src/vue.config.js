db1e1bd5ec0b4191520515a0ff5670045b35d3e4
Pawat Saengduan <49898465+pawatsaengduan@users.noreply.github.com>
2020-04-10 17:41:36 +0700

    Lint the files

--- frontend/src/vue.config.js
+++ frontend/src/vue.config.js
@@ -1,16 +1,14 @@
 module.exports = {
-    "transpileDependencies": [
-        "vuetify"
-    ],
-    devServer: {
-        proxy: {
-            '/': {
-                target: 'http://localhost:39112/',
-                'changeOrigin': true,
-                'secure': false
-            }
-        }
-    },
-    // outputDir: './dist/',
-    assetsDir: 'static',
+	transpileDependencies: ['vuetify'],
+	devServer: {
+		proxy: {
+			'/': {
+				target: 'http://127.0.0.1:39112/',
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
+	// outputDir: './dist/',
+	assetsDir: 'static',
 };
