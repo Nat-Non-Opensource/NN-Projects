a02b50c5d4f5e64d99f1171867b5fab05b1b58f9
pawatsaengduan <saengduanpawat@gmail.com>
2020-04-20 17:51:54 +0700

    Remove all comments

--- backend/src/mapapp/urls.py
+++ backend/src/mapapp/urls.py
@@ -1,4 +1,4 @@
-# from django.urls import path, include
+from django.urls import path #, include
 # from rest_framework import routers
 from django.conf.urls import url
 
@@ -11,5 +11,6 @@ urlpatterns = [
     # path('', views.index, name="index")
     # path('', include(router.urls))
     url(r'^$', views.map_list),
-    url(r'^(?P<pk>[0-9]+)$', views.map_detail)
+    url(r'^(?P<pk>[0-9]+)$', views.map_detail),
+    # path('images', views.display_place_images, name = 'placeImages'),
 ]
