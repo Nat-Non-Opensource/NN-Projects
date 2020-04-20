150080aaaf26991e9beb73e5ef40d3f83cf64101
pawatsaengduan <saengduanpawat@gmail.com>
2020-04-20 17:54:37 +0700

    Add a /media access

--- backend/src/src/urls.py
+++ backend/src/src/urls.py
@@ -17,9 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls import url
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
 	path('map/', include('mapapp.urls')),
 	path('admin/', admin.site.urls),
-	url(r'^$', TemplateView.as_view(template_name='index.html')),
 ]
+
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
