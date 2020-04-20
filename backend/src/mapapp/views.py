1ea4dab41453f1155bc9c1d002af5919132eff17
pawatsaengduan <saengduanpawat@gmail.com>
2020-04-20 17:52:42 +0700

    Design a new comments

--- backend/src/mapapp/views.py
+++ backend/src/mapapp/views.py
@@ -1,11 +1,13 @@
 import json
 from django.http import HttpResponse
-from .models import Map
+from .models import *
 from .serializers import MapSerializer
 from rest_framework import viewsets, status
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
+from .forms import *
 
 
 # Create your views here.
@@ -18,16 +20,16 @@ class MapViewSet(viewsets.ModelViewSet):
     serializer_class = MapSerializer
 
 
-# API
+# API.
 @csrf_exempt
 def map_list(request):
-    # Get list
+    # Get list.
     if request.method == 'GET':
         maps = Map.objects.all()
         maps_serializer = MapSerializer(maps, many=True)
         return JsonResponse(maps_serializer.data, safe=False)
 
-    # Add one
+    # Add one.
     if request.method == 'POST':
         map_data = JSONParser().parse(request)
         maps_serializer = MapSerializer(data=map_data)
@@ -36,7 +38,7 @@ def map_list(request):
             return JsonResponse(maps_serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(maps_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    # Delete list
+    # Delete list.
     if request.method == 'DELETE':
         Map.objects.all().delete()
         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
@@ -49,12 +51,12 @@ def map_detail(request, pk):
     except mapping.DoesNotExist:
         return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
-    # Retrieve one record
+    # Retrieve one record.
     if request.method == 'GET':
         map_serializer = MapSerializer(mapping)
         return JsonResponse(map_serializer.data)
 
-    # Update one record
+    # Update one record.
     if request.method == 'PUT':
         map_data = JSONParser().parser(request)
         map_serializer = MapSerializer(mapping, data=map_data)
@@ -63,7 +65,7 @@ def map_detail(request, pk):
             return JsonResponse(map_serializer.data)
         return JsonResponse(map_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    # Delete one record
+    # Delete one record.
     if request.method == 'DELETE':
         Map.delete()
         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
