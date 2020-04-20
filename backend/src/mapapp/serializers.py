012343cb85315760b87d4bdc07ce81f146ad9785
pawatsaengduan <saengduanpawat@gmail.com>
2020-04-20 17:51:28 +0700

    Remove all comments

--- backend/src/mapapp/serializers.py
+++ backend/src/mapapp/serializers.py
@@ -1,12 +1,12 @@
 from .models import Map
-from .models import Map_Image
+# from .models import Map_Image
 from rest_framework import serializers
 
 
 class MapSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Map
-        fields = '__all__'
+        fields = ['place_name', 'place_categories', 'place_description', 'place_enable', 'place_latitude', 'place_longitude']
 
 
 # class MapImageSerializer(serializers, HyperlinkedModelSerializer):
