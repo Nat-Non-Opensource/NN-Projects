a3800e925b6330216d67ec83149290c890123544
pawatsaengduan <saengduanpawat@gmail.com>
2020-04-20 17:53:32 +0700

    Remove all comments

--- backend/src/mapapp/serializers.py
+++ backend/src/mapapp/serializers.py
@@ -6,4 +6,4 @@ class MapSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Map
         fields = ['place_name', 'place_categories', 'place_description', 'place_enable', 'place_latitude', 'place_longitude']
-        
+    
